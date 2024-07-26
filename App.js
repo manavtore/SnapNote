e0ecdb804445f6e0b8181ce0b722b05c678bc5ea
manavtore <113475112+manavtore@users.noreply.github.com>
2024-07-26 16:28:39 +0530

    fix:allNotesScreen gesturecontroller

--- App.js
+++ App.js
@@ -1,31 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './components/Header';
-import { useState } from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Homescreen from './screens/HomeScreen';
-import AddNotesScreen from './screens/AddNoteScreen.js';
-import AllnotesScreen from './screens/AllnotesScreen.js';
-import Backbutton from './components/Backbutton.js';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import Header from "./components/Header";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Homescreen from "./screens/HomeScreen";
+import AddNotesScreen from "./screens/AddNoteScreen";
+import AllnotesScreen from "./screens/AllnotesScreen";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Header/>
-     {/* <Backbutton/>  */}
+    <GestureHandlerRootView style={styles.container}>
+      <Header />
       <NavigationContainer>
         <Stack.Navigator>
-        <Stack.Screen  name="Home" component={Homescreen}/>
-        <Stack.Screen name='addNoteScreen' component={AddNotesScreen}/>
-         <Stack.Screen name='allNoteScreen' component={AllnotesScreen}/>
+          <Stack.Screen name="Home" component={Homescreen} />
+          <Stack.Screen name="addNoteScreen" component={AddNotesScreen} />
+          <Stack.Screen name="allNoteScreen" component={AllnotesScreen} />
         </Stack.Navigator>
       </NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
--- components/card.js
+++ components/card.js
@@ -2,54 +2,74 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
-import {  Card,Button, Title, Paragraph } from "react-native-paper";
+import { Card, Button, Title, Paragraph } from "react-native-paper";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
 const NoteCard = () => {
+  const [notes, setNotes] = useState([]);
+  const navigation = useNavigation();
 
-    const[note,setNotes] = useState([]);
+  useEffect(() => {
+    getNotes();
+  }, []);
 
-    useEffect(() => {
-        getUser();
-    }, []);
+  const getNotes = () => {
+    axios
+      .get("http://localhost/notesapp/index.php/${id}/notes")
+      .then((response) => {
+        setNotes(response.data);
+      })
+      .catch((error) => {
+        alert(response.data.message);
+      });
+  };
 
-    function getUser(){
-        axios.get('http://localhost/notesapp/index.php').then(function(response){
-            // alert(response.data);
-             setNotes(response.data);
-        })
-        .catch((error) =>{
-            alert(error);
-        });
-        
-    }
+  const deleteNote = (id) => {
+    axios
+      .delete(`http://localhost/notesapp/index.php/${id}/delete`)
+      .then((response) => {
+        alert("Note Deleted");
+        getNotes();
+      })
+      .catch((error) => {
+        alert(error);
+        console.error(error);
+      });
+  };
+
+  // const editNote = (id) => {
+  //   navigation.navigate("editNotesScreen", { id });
+  // };
+
   return (
-    <>
-    <FlatList
-        data={note}
-        renderItem={({ item}) => (
-            <Card style={style.Card}>
-            <Card.Title title={note.title} />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <FlatList
+        data={notes}
+        renderItem={({ item }) => (
+          <Card style={styles.card} key={item.id}>
+            <Card.Title title={item.title} subtitle={`ID: ${item.id}`} />
             <Card.Content>
-              <Paragraph>${note.note}</Paragraph>
+              <Paragraph>{item.note}</Paragraph>
             </Card.Content>
             <Card.Actions>
-              <Button>Edit</Button>
-              <Button>Delete</Button>
+              <Button onPress={() => editNote(item.id)}>Edit</Button>
+              <Button onPress={() => deleteNote(item.id)}>Delete</Button>
             </Card.Actions>
           </Card>
-
         )}
         keyExtractor={(item) => item.id.toString()}
-    />
-    </>
-    
+        onEndReached={getNotes}
+        onEndReachedThreshold={0}
+      />
+    </GestureHandlerRootView>
   );
 };
 
 export default NoteCard;
 
-const style = StyleSheet.create({
-  Card: {
+const styles = StyleSheet.create({
+  card: {
     marginBottom: 15,
   },
 });
